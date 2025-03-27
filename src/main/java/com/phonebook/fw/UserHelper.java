3c34c780e31c5eea39e47164a3d2dadfd1940443
cohen <cohen0402@gmail.com>
2025-03-27 22:42:30 +0200

    Phonebook refactoring and class of object Helper AppManager

---
+++ src/main/java/com/phonebook/fw/UserHelper.java
@@ -0,0 +1,38 @@
+package com.phonebook.fw;
+
+import com.phonebook.fw.BaseHelper;
+import com.phonebook.models.User;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class UserHelper extends BaseHelper {
+    public UserHelper(WebDriver driver) {
+        super(driver);
+    }
+
+    public void clickOnRegistrationButton() {
+        click(By.name("registration"));
+    }
+
+    public void fillRegisterLoginForm(User user) {
+        type(By.name("email"), user.getMail());
+        // enter password
+        type(By.name("password"), user.getPassword());
+    }
+
+    public void clickOnLoginLink() {
+        click(By.cssSelector("[href='/login']"));
+    }
+
+    public boolean isSignButtonPresent() {
+        return isElementPresent(By.xpath("//button[.='Sign Out']"));
+    }
+
+    public void clicOnLoginButton() {
+        click(By.name("login"));
+    }
+
+    public boolean isErrorMessagePresent() {
+        return isElementPresent(By.cssSelector(".login_login__3EHKB>div"));
+    }
+}
